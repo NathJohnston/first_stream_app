f4c51f21dbd6da67804202aa02841f1fd8ceab32
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-06-21 05:09:10 +0800

    Update streamlit_app.py
    Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,15 +28,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New Section to display furuityvice api response
-#streamlit.header('Fruityvice Truit Advice!')
+streamlit.header('Fruityvice Truit Advice!')
+fruit_choice = streamlit.text_input("What fruit would you like information about?, 'kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response.json()) #just writes the data on the screen                                    
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-#streamlit.text(fruityvice_response.json()) #just writes the data on the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)                                    
+
 
 
 # take the json version of response and normalise it
-#fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
 # output it to the screen as a table
-#streamlit.dataframe(fruityvice_normalised)
+streamlit.dataframe(fruityvice_normalised)
