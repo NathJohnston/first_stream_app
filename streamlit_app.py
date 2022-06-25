3e4c51eb8b761c2421fbbd52a78e4a226ac953c3
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-06-26 07:42:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 import requests
 import snowflake.connector
 #use this for Control of Flow changes - error message handling
-from urllib.error import URLERROR
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
