7eb1e2298d78b9e8942742a4ddccfcb073b72542
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-06-26 16:57:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
+/* original code without function
 #New Section to display furuityvice api response
 streamlit.header('Fruityvice Truit Advice!')
 try:
@@ -47,7 +48,7 @@ try:
       streamlit.dataframe(fruityvice_normalised)
 except URLError as e:
     streamlit.error()
-
+*/
 streamlit.write('The user entered', fruit_choice)
 
 
