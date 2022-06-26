933c525a5f5e25e855e6267f6791194b07459655
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-06-26 16:37:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ streamlit.header('Fruityvice Truit Advice!')
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
-      stremlit.error("Please select a fruit to get information.")
+      streamlit.error("Please select a fruit to get information.")
    else:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)   
       # take the json version of response and normalise it
