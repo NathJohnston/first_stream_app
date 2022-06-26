01c1970c9a66ed701a1ee74a66b57ef8d4e8c934
NathJohnston <77366902+NathJohnston@users.noreply.github.com>
2022-06-26 17:06:50 +0800

    Update streamlit_app.py
    Create get_fruitvaice_data function

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,22 +33,27 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
-/* original code without function
+# Create the repeatable code block Function
+def get_fruitvice_data(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)   
+   # take the json version of response and normalise it
+   fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalised
+
 #New Section to display furuityvice api response
 streamlit.header('Fruityvice Truit Advice!')
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
-   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)   
-      # take the json version of response and normalise it
-      fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+   else:     
       # output it to the screen as a table
-      streamlit.dataframe(fruityvice_normalised)
+      back_from_function = get_fruitvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
+      
 except URLError as e:
     streamlit.error()
-*/
+
 streamlit.write('The user entered', fruit_choice)
 
 
